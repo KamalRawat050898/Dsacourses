ea86ab2693172bd87d771251ce507d63added0fb
kamalRawat050898 <sharma66329525@gmail.com>
2024-03-29 21:38:49 -0700

    my first commit

---
+++ if.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int time;
+    cout<<"enter the time : ";time;
+    cin>>time;
+    if((time>12) && (time<=12))
+    {
+        cout<<"good morning"
+    }
+}
